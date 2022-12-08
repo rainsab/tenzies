e561a1476f1269a6d14ddc57e16cc8597a49af42
rainsab <m.basniar@gmail.com>
2022-12-09 00:11:27 +0100

    made basic setup for tenzies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Die from "./Die"
 
 export default function App() {
   return (
-    <>
-      <h1>Tenzies</h1>
-    </>
+    <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
   )
 }
