a50e52a920d4921566234fef4c067d752be912a4
rainsab <m.basniar@gmail.com>
2022-12-09 14:50:25 +0100

    instaled nanoid and convert dice into array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import React, { useState } from 'react'
+import { nanoid } from "nanoid"
 import Die from "./Die"
 
 export default function App() {
-  const [dice, setDice] = useState(newDiceGrid())
+    const [dice, setDice] = useState(newDiceGrid())
 
-  //arrow function?
-  function newDiceGrid() {
-      const newDice = []
-      for (let i = 0; i < 10; i++) {
-          newDice.push(Math.ceil(Math.random() * 6)) //floor, ceil, etc.
-      }
-      return newDice
-  }
+    //arrow function?
+    function newDiceGrid() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), //floor, ceil, etc.
+                isHeld: false,
+                id: nanoid()
+            })
+        }
+        console.log(newDice)
+        return newDice;
+    }
 
-  function rollDice() {
-    setDice(newDiceGrid())
-  }
-  
-  const diceElements = dice.map(die => <Die value={die} />)
-  
-  return (
-      <main>
-          <div className="dice-container">
-              {diceElements}
-          </div>
-          <button className="roll-dice" onClick={rollDice}>Roll</button>
-      </main>
-  )
+    function rollDice() {
+        setDice(newDiceGrid());
+    }
+
+    const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
+
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
 }
