96e036b4cfd6efdd8b993c43c84d3cabae28f954
rainsab <m.basniar@gmail.com>
2022-12-09 18:46:45 +0100

    made enable to play new game when win

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,11 @@ export default function App() {
     const [tenzies, setTenzies] = useState(false);
 
     useEffect(() => {
-        const allHeld = dice.every(die => die.isHeld)
-        const allSameValue = dice.every(die => die.value === dice[0].value)
+        const allHeld = dice.every(die => die.isHeld);
+        const allSameValue = dice.every(die => die.value === dice[0].value);
+
         if (allHeld && allSameValue) {
-            setTenzies(true)
-            console.log("You won!")
+            setTenzies(true);
         }
     }, [dice])
     
@@ -37,10 +37,15 @@ export default function App() {
         }
     }
 
-    const rollDice = () => {
-        setDice(oldDice => oldDice.map(die => {
-            return die.isHeld ? die : generateNewDie();
-        }))
+    const button = () => {
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? die : generateNewDie();
+            }))
+        } else {
+            setTenzies(false);
+            setDice(newDiceGrid());
+        }
     }
 
     const holdDice = (id) => {
@@ -49,14 +54,20 @@ export default function App() {
         }))
     }
 
-    const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}
+        />
+    ))
 
     return (
         <main>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice" onClick={button}>
+                {tenzies ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -7,11 +7,11 @@ export default function Die(props) {
 
     return (
         <div
-            className="die-face"
+            className="die-ui"
             style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
+            <h2 className="die-number">{props.value}</h2>
         </div>
     )
 }
