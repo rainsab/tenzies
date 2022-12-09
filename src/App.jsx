e6dd8d47f886bbdc7f102d7faa569f72e32e4e58
rainsab <m.basniar@gmail.com>
2022-12-09 17:13:24 +0100

    made dice array winning conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { nanoid } from "nanoid"
 import Die from "./Die"
 
 export default function App() {
-    const [dice, setDice] = useState(newDiceGrid())
-
-    //arrow function?
-    function newDiceGrid() {
+    const newDiceGrid = () => {
         const newDice = []
         for (let i = 0; i < 10; i++) {
             newDice.push({
-                value: Math.ceil(Math.random() * 6), //floor, ceil, etc.
+                value: Math.floor(Math.random() * 6) + 1, //array(10).fill().map()
                 isHeld: false,
                 id: nanoid()
             })
         }
-        console.log(newDice)
         return newDice;
     }
+    
+    const [dice, setDice] = useState(newDiceGrid());
+    
+    const [tenzies, setTenzies] = useState(false);
 
-    function rollDice() {
-        setDice(newDiceGrid());
+    useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const allSameValue = dice.every(die => die.value === dice[0].value)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+            console.log("You won!")
+        }
+    }, [dice])
+    
+
+    const generateNewDie = () => {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
     }
 
-    const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
+    const rollDice = () => {
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? die : generateNewDie();
+        }))
+    }
+
+    const holdDice = (id) => {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? {...die, isHeld: !die.isHeld} : die
+        }))
+    }
+
+    const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
     return (
         <main>
