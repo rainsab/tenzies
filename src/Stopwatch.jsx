940182ac2a70a074fbf78defc9930b066f8e70ee
rainsab <m.basniar@gmail.com>
2022-12-13 12:19:18 +0100

    updated format of time with dot before milliseconds

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -38,21 +38,21 @@ export default function Stopwatch(props) {
         <div className="stopwatch-container">
             <div className="stopwatch">
                 <span>{("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:</span>
-                <span>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}:</span>
+                <span>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.</span>
                 <span>{("0" + ((props.time / 10) % 100)).slice(-2)}</span>
             </div>
 
-            <p>Best time:
+            <div>Best time:
                 {bestTime === Infinity ?
                     <span className="span-gap">not played yet</span>
                     :
                     <span className="span-gap">
                         <span>{("0" + Math.floor((bestTime / 60000) % 60)).slice(-2)}:</span>
-                        <span>{("0" + Math.floor((bestTime / 1000) % 60)).slice(-2)}:</span>
+                        <span>{("0" + Math.floor((bestTime / 1000) % 60)).slice(-2)}.</span>
                         <span>{("0" + ((bestTime / 10) % 100)).slice(-2)}</span>
                     </span>
                 }
-            </p>
+            </div>
         </div>
     )
 }
