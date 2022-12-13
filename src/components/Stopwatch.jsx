cf7260b4be5d0ebf2054c5f9978584a12e141a7f
rainsab <m.basniar@gmail.com>
2022-12-13 16:53:44 +0100

    updated stopwatch with tittle

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -29,10 +29,12 @@ export default function Stopwatch(props) {
 
     return (
         <div className="text-center">
-            <div className="stopwatch">
-                <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}.</span>
-                <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+            <div>Stopwatch:
+                <span className="stopwatch span-gap">
+                    <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+                    <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}.</span>
+                    <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+                </span>
             </div>
 
             <div>Best time:
